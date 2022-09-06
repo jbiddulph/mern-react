b18d0e21101b279cbf9db02f6bb240df603d4e72
jbiddulph <john.mbiddulph@gmail.com>
2022-09-06 23:48:05 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavbarBrand,
+} from "mdb-react-ui-kit";
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "../redux/features/authSlice";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+  };
+  const { user } = useSelector((state) => ({ ...state.auth }));
+  return (
+    <MDBNavbar fixed="top" expand="lg" style={{ backgroundColor: "#f0e6ea" }}>
+      <MDBContainer>
+        <MDBNavbarBrand
+          href="/"
+          style={{ color: "#606080", fontWeight: "600", fontSize: "22px" }}
+        >
+          JBIDDULPH
+        </MDBNavbarBrand>
+        <MDBNavbarToggler
+          type="button"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setShow(!show)}
+        >
+          <MDBIcon icon="bars" fas style={{ color: "#606080" }} />
+        </MDBNavbarToggler>
+        <MDBCollapse show={show} navbar>
+          <MDBNavbarNav right fullWidth={false} className="mb-2  mb-lg-0">
+            {user?.result?._id && (
+              <h5 style={{ marginRight: "30px", marginTop: "17px" }}>
+                Logged in as: {user?.result?.name}
+              </h5>
+            )}
+            <MDBNavbarItem>
+              <MDBNavbarLink href="/">
+                <p className="header-text">Home</p>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            {user?.result?._id && (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/addItem">
+                    <p className="header-text">Add Item</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/dashboard">
+                    <p className="header-text">Dashboard</p>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+              </>
+            )}
+            {user?.result?._id ? (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/login">
+                  <p className="header-text" onClick={() => handleLogout()}>
+                    Logout
+                  </p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            ) : (
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/login">
+                  <p className="header-text">Login</p>
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+            )}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+};
+
+export default Header;
---
+++ src/redux/api.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const API = axios.create({ baseURL: "http://localhost:5050" });
+
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
+export const googleSignIn = (result) => API.post("/users/googleSignIn", result);
